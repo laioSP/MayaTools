9ec8338ffe7cc255ef5f3085224f0e4cbb62125e
laioSP <60949953+laioSP@users.noreply.github.com>
2021-04-18 16:12:19 -0700

    Update jointOrientOptions.py

--- jointOrientOptions.py
+++ jointOrientOptions.py
@@ -1,20 +1,32 @@
-import maya.cmds as cmds
-currentSelection=cmds.ls(sl=True,type='joint')
-for x in range(len(currentSelection)):
-    cmds.parent(currentSelection[x+1],currentSelection[x])
-    if x + 2 == len(currentSelection):
-        break
-cmds.select(currentSelection[0])
-cmds.joint(e=True,oj='xyz',sao='xup',ch=True,zso=True)
+from maya import cmds
 
-oritentX=cmds.getAttr(str(currentSelection[-2])+".jointOrientX")
-oritentY=cmds.getAttr(str(currentSelection[-2])+".jointOrientY")
-oritentZ=cmds.getAttr(str(currentSelection[-2])+".jointOrientZ")
+def allAxes():
+    allJoints = cmds.ls(type='joint')
+    check = cmds.getAttr(allJoints[0] + ".displayLocalAxis")
+    switch =  abs(check - 1)        
+    for x in allJoints:        
+        cmds.setAttr(x + ".displayLocalAxis", switch)
+        
+def orientUnparented():
+    currentSelection=cmds.ls(sl=True,type='joint')
+    for x in range(len(currentSelection)):
+        cmds.parent(currentSelection[x+1],currentSelection[x])
+        if x + 2 == len(currentSelection):
+            break
+    cmds.select(currentSelection[0])
+    cmds.joint(e=True,oj='xyz',sao='xup',ch=True,zso=True)  
+    oritentX=cmds.getAttr(str(currentSelection[-2])+".jointOrientX")
+    oritentY=cmds.getAttr(str(currentSelection[-2])+".jointOrientY")
+    oritentZ=cmds.getAttr(str(currentSelection[-2])+".jointOrientZ")
+    cmds.setAttr(str(currentSelection[-1])+".jointOrientX",oritentX)
+    cmds.setAttr(str(currentSelection[-1])+".jointOrientY",oritentY)
+    cmds.setAttr(str(currentSelection[-1])+".jointOrientZ",oritentZ)
+    
+    for x in currentSelection:
+        if x != currentSelection[0]:
+            cmds.parent(x,w=True)
 
-cmds.setAttr(str(currentSelection[-1])+".jointOrientX",oritentX)
-cmds.setAttr(str(currentSelection[-1])+".jointOrientY",oritentY)
-cmds.setAttr(str(currentSelection[-1])+".jointOrientZ",oritentZ)
-
-for x in currentSelection:
-    if x != currentSelection[0]:
-        cmds.parent(x,w=True)
+cmds.OrientJointOptions()
+mod = cmds.frameLayout(l='extra tools', cll = True, bgc = (0.3,0.4,0.6))
+cmds.button( l = 'Toggle All Axes Visibility', c='allAxes()', p=mod, bgc = (0.3,0.3,0.4) )
+cmds.button( l = 'Orient Unparented According to selection ',c = 'orientUnparented()',  p=mod, bgc = (0.3,0.3,0.4) )
