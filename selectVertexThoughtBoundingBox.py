3d16487a55b9e17fa3e573c44c0f0293e5154862
laioSP <60949953+laioSP@users.noreply.github.com>
2021-05-15 17:21:59 -0700

    Update selectVertexThoughtBoundingBox.py

--- selectVertexThoughtBoundingBox.py
+++ selectVertexThoughtBoundingBox.py
@@ -1,7 +1,17 @@
 from maya import cmds
-cages=cmds.ls(sl=1)
-objList=cmds.ls(sl=1)
+collidingCages = []
+objList = []
 
+def screening():
+    allObjs=cmds.ls(type='mesh')
+    for obj in allObjs:
+        for frame in cages:
+            if obj not in cages:
+                check = verify(frame, obj)           
+                if check:
+                    collidingCages.append(check[1])
+                    objList.append(check[0])
+    
 def getVertices():
     vertices=[]
     for x in objList:
@@ -10,96 +20,75 @@ def getVertices():
         for v in range(int(getRange[0]),int(getRange[1])):
             vertices.append(x+".vtx["+str(v)+"]")
     return vertices
-    
-def verify(frame, point):
 
-    getBox = cmds.exactWorldBoundingBox( frame)
-    xMax,yMax, zMax, xMin, yMin, zMin = getBox
-    getPosition = cmds.xform( point, q=True, ws=True, t=True)
-    xPos, yPos, zPos = getPosition
-    if xMin >= xPos:
-        if xMax <= xPos:
-            if yMin >= yPos:
-                if yMax <= yPos:
-                    if zMin >= zPos:
-                        if zMax <= zPos:
-                            return frame
+def getLocation(input):
+    if 'Shape' in input:
+        inputTransform = input.split('Shape')[0] + input.split('Shape')[1]
+        input = inputTransform
+        xMax,yMax, zMax, xMin, yMin, zMin = cmds.exactWorldBoundingBox( input ) 
+        return (input, xMax, yMax, zMax, xMin, yMin, zMin) 
+        
+    if cmds.objectType(input) == 'transform':        
+        if cmds.objectType(cmds.ls(input,dag=1)[1]) == 'mesh':
+            xMax,yMax, zMax, xMin, yMin, zMin = cmds.exactWorldBoundingBox( input )
+            return (input, xMax, yMax, zMax, xMin, yMin, zMin) 
+    if ".vtx[" in input:
+        temp = cmds.xform( input, q=True, ws=True, t=True)
+        xMax, yMax, zMax = temp
+        xMin, yMin, zMin = temp
+        return (input, xMax, yMax, zMax, xMin, yMin, zMin)       
+
+def verify(frame, point):
+    result = []
+
+    pointObj, xMax,yMax, zMax, xMin, yMin, zMin = getLocation(frame)
+    frameObj, xPosMax, yPosMax, zPosMax, xPosMin, yPosMin, zPosMin = getLocation(point)  
+
+    if xMin >= xPosMin:
+        result.append(frameObj)
+        result.append(pointObj)
+        result.append(xMin)
+        if xMax <= xPosMax:
+            result[2]=xMin, xMax
+            if yMin >= yPosMin:
+                result[2]=xMin, xMax, yMin
+                if yMax <= yPosMax:
+                    result[2]=xMin, xMax, yMin, yMax
+                    if zMin >= zPosMin:
+                        result[2]=xMin, xMax, yMin, yMax, zMin
+                        if zMax <= zPosMax:
+                            result[2]=xMin, xMax, yMin, yMax, zMin, zMax
+                            return result
+                        else:
+                            return result
+                    else:
+                        return result
+                else:
+                    return result
+            else:
+                return result
 
 def collectVertices():
+    screening()
     vertices = getVertices()
-    for frame in cages:
+    for frame in collidingCages:
         for vertice in vertices:
             Filter = verify(frame, vertice)
-            if Filter:
-                setName = Filter+'_set_'
-                counter = (len(cmds.ls(setName+'*')) / 2)+1
-                if counter < 10:
-                    counter = '0' + str(counter)
-                else:
-                    counter = str(counter)
-                print counter                   
-                if cmds.ls(setName + counter):
-                    cmds.sets(vertice, add=setName + str(counter))
-                else:    
-                    cmds.sets(vertice, n=setName + counter)
+            if Filter:     
+                if len(Filter[2]) == 6 :
+                    print Filter
+                    setName = Filter[1]+'_set_'
+                    counter = (len(cmds.ls(setName+'*')) / 2)+1
 
-for x in objList:
-    print x
-    getVtx = cmds.ls(x+".vtx[*]")[0]
-    getRange = getVtx.split('[')[-1].split(']')[0].split(':')
-    for v in range(int(getRange[0]),int(getRange[1])):
-        vertices.append(x+".vtx["+str(v)+"]")
+                    if counter < 10:
+                        counter = '0' + str(counter)
+                    else:
+                        counter = str(counter)
+                    print counter       
+                    if cmds.ls(setName + counter):
+                        cmds.sets(vertice, add=setName + counter)
+                    else:    
+                        cmds.sets(vertice, n=setName + counter)
 
-collectVertices()from maya import cmds
-cages=cmds.ls(sl=1)
-objList=cmds.ls(sl=1)
-
-def getVertices():
-    vertices=[]
-    for x in objList:
-        getVtx = cmds.ls(x+".vtx[*]")[0]
-        getRange = getVtx.split('[')[-1].split(']')[0].split(':')
-        for v in range(int(getRange[0]),int(getRange[1])):
-            vertices.append(x+".vtx["+str(v)+"]")
-    return vertices
-    
-def verify(frame, point):
-
-    getBox = cmds.exactWorldBoundingBox( frame)
-    xMax,yMax, zMax, xMin, yMin, zMin = getBox
-    getPosition = cmds.xform( point, q=True, ws=True, t=True)
-    xPos, yPos, zPos = getPosition
-    if xMin >= xPos:
-        if xMax <= xPos:
-            if yMin >= yPos:
-                if yMax <= yPos:
-                    if zMin >= zPos:
-                        if zMax <= zPos:
-                            return frame
-
-def collectVertices():
-    vertices = getVertices()
-    for frame in cages:
-        for vertice in vertices:
-            Filter = verify(frame, vertice)
-            if Filter:
-                setName = Filter+'_set_'
-                counter = (len(cmds.ls(setName+'*')) / 2)+1
-                if counter < 10:
-                    counter = '0' + str(counter)
-                else:
-                    counter = str(counter)
-                print counter                   
-                if cmds.ls(setName + counter):
-                    cmds.sets(vertice, add=setName + str(counter))
-                else:    
-                    cmds.sets(vertice, n=setName + counter)
-
-for x in objList:
-    print x
-    getVtx = cmds.ls(x+".vtx[*]")[0]
-    getRange = getVtx.split('[')[-1].split(']')[0].split(':')
-    for v in range(int(getRange[0]),int(getRange[1])):
-        vertices.append(x+".vtx["+str(v)+"]")
-
-collectVertices()
+cages=cmds.ls(sl=1, dag=1, type='mesh')        
+collectVertices()  
