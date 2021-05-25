05906c0fc3d18dd8bd6f7c89394eaa271effee36
laioSP <60949953+laioSP@users.noreply.github.com>
2021-05-24 18:13:48 -0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,7 @@
 from maya import cmds
 import pymel.core as pm
 
-driven = 'joint1' 
-driver = cmds.ls(sl=1)[0]
 shape=pm.selected()[0].split('.')[0]
-
 
 def getRange(currentSelection):
     list = currentSelection.split('[')[-1].split(']')[0].split(':')
@@ -13,19 +10,14 @@ def getRange(currentSelection):
 def getPositions(rootPosition):   
     xPos=[]; yPos=[]; zPos=[]
     for x in set(cmds.listAttr(rootPosition)) - set(cmds.listAttr(rootPosition, s=1)):
-        fullAttr = cmds.getAttr(shape + '.' + x)[0]
+        fullAttr = cmds.xform(shape + '.' + x, q=True, ws=True, t=True)        
         xPos.append(fullAttr[0]); yPos.append(fullAttr[1]); zPos.append(fullAttr[2])
     return (sum(xPos)/len(xPos), sum(yPos)/len(yPos), sum(zPos)/len(zPos))
 
-driven = 'joint1' 
-driver = cmds.ls(sl=1)[0]
-
-def match(driven, driver):
-    driverPos = getPositions(driver)
+def match(driver):
+    print driver
+    print getPositions(driver)
+    cmds.select(cl=1)
+    cmds.joint( n=shape.split('Shape')[0]+'_JNT', p=getPositions(driver) )
     
-    print driverPos
-    cmds.setAttr(driven+'.translateX',driverPos[0])
-    cmds.setAttr(driven+'.translateY',driverPos[1])
-    cmds.setAttr(driven+'.translateZ',driverPos[2])
-    
-match(driven, driver)
+match(cmds.ls(sl=1)[0])
